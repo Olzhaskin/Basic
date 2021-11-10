2dfc61e46880fa3159679c2173dae2fbdd92cd19
Olzhaskin <oljik132@gmail.com>
2021-11-11 00:00:41 +0600

    Changing 1st section, adding 2nd, 3rd, 4th, 5th sections

--- js/index.js
+++ js/index.js
@@ -2,5 +2,81 @@ let navbarHeight = document.getElementById('nb').offsetHeight;
 let firstSectionWrapper = document.getElementById('first-section-wrapper');
 let onepixel = 1;
 let rowFullHeight = document.getElementById('row-full-height');
+let firstSectionCarousel = document.getElementById('first-section-carousel');
+// let onionsImage = document.getElementById('onions-image');
+// let soybeansImage = document.getElementById('soybeans-image');
+// let sunseedsImage = document.getElementById('sunseeds-image');
+// let wheatImage = document.getElementById('wheat-image');
+// let watermelonsImage = document.getElementById('watermelons-image');
+// let beetrootsImage = document.getElementById('beetroots-image');
+// let tomatoesImage = document.getElementById('tomatoes-image');
+// let cucumbersImage = document.getElementById('cucumbers-image');
+// let potatoesImage = document.getElementById('potatoes-image');
+// let cornImage = document.getElementById('corn-image');
 firstSectionWrapper.style.height = window.innerHeight - navbarHeight - onepixel + 'px';
 rowFullHeight.style.height = firstSectionWrapper.style.height;
+firstSectionCarousel.style.height = firstSectionWrapper.style.height;
+// function onHover() {
+//     if (event.target.id == 'onions-image') {
+//         onionsImage.src = 'images/onion-open.jpg';
+//     }
+//     else if (event.target.id == 'soybeans-image') {
+//         soybeansImage.src = 'images/soybean-open.jpg';
+//     }
+//     else if (event.target.id == 'sunseeds-image') {
+//         sunseedsImage.src = 'images/sunseed-open.jpg';
+//     }
+//     else if (event.target.id == 'wheat-image') {
+//         wheatImage.src = 'images/wheat-open.jpg';
+//     }
+//     else if (event.target.id == 'watermelons-image') {
+//         watermelonsImage.src = 'images/watermelon-open.jpg';
+//     }
+//     else if (event.target.id == 'beetroots-image') {
+//         beetrootsImage.src = 'images/beetroot-open.jpg';
+//     }
+//     else if (event.target.id == 'tomatoes-image') {
+//         tomatoesImage.src = 'images/tomato-open.jpg';
+//     }
+//     else if (event.target.id == 'cucumbers-image') {
+//         cucumbersImage.src = 'images/cucumber-open.jpg';
+//     }
+//     else if (event.target.id == 'potatoes-image') {
+//         potatoesImage.src = 'images/potato-open.jpg';
+//     }
+//     else if (event.target.id == 'corn-image') {
+//         cornImage.src = 'images/corn-open.jpg';
+//     }
+// }
+// function offHover() {
+//     if (event.target.id == 'onions-image') {
+//         onionsImage.src = 'images/onions.jpg';
+//     }
+//     else if (event.target.id == 'soybeans-image') {
+//         soybeansImage.src = 'images/soybeans.jpg';
+//     }
+//     else if (event.target.id == 'sunseeds-image') {
+//         sunseedsImage.src = 'images/sunflower-seeds.jpg';
+//     }
+//     else if (event.target.id == 'wheat-image') {
+//         wheatImage.src = 'images/wheat-seeds.jpg';
+//     }
+//     else if (event.target.id == 'watermelons-image') {
+//         watermelonsImage.src = 'images/watermelons.jpg';
+//     }
+//     else if (event.target.id == 'beetroots-image') {
+//         beetrootsImage.src = 'images/beetroots.jpg';
+//     }
+//     else if (event.target.id == 'tomatoes-image') {
+//         tomatoesImage.src = 'images/tomatoes.jpg';
+//     }
+//     else if (event.target.id == 'cucumbers-image') {
+//         cucumbersImage.src = 'images/cucumbers.jpg';
+//     }
+//     else if (event.target.id == 'potatoes-image') {
+//         potatoesImage.src = 'images/potatoes.jpg';
+//     }
+//     else if (event.target.id == 'corn-image') {
+//         cornImage.src = 'images/corn.jpg';
+//     }
+// }
